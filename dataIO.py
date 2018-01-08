207332dff59276aee26e53e67a3cc89eeffd3b3b
Landon <xuezhp@shanghaitech.edu.cn>
2018-01-08 20:36:54 +0800

    update

--- dataIO.py
+++ dataIO.py
@@ -16,8 +16,8 @@ except:
     pass
     print 'All dependencies not loaded, some functionality may not work'
 
-LOCAL_PATH = '/home/meetshah1995/datasets/ModelNet/3DShapeNets/volumetric_data/'
-SERVER_PATH = '/home/gpu_users/meetshah/3dgan/volumetric_data/'
+LOCAL_PATH = './3DShapeNets/volumetric_data/'
+# SERVER_PATH = '/home/gpu_users/meetshah/3dgan/volumetric_data/'
 
 def getVF(path):
     raw_data = tuple(open(path, 'r'))
